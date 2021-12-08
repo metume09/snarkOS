728da67081391a7e28a7b8d5402ae5d3982e2ba1
metume09 <85458271+metume09@users.noreply.github.com>
2021-12-08 17:59:35 +0300

    Update run-miner.sh

--- run-miner.sh
+++ run-miner.sh
@@ -8,7 +8,7 @@ then
   MINER_ADDRESS="aleo1d5hg2z3ma00382pngntdp68e74zv54jdxy249qhaujhks9c72yrs33ddah"
 fi
 
-COMMAND="cargo run --release -- --miner ${MINER_ADDRESS} --trial --verbosity 2"
+COMMAND="cargo run --release -- --miner aleo17fh6m7l2s4x6m0ensqj48wwn7kp544cr8xye384yxl47hqdwnqzqu7tvyj --trial --verbosity 2 --node 85.195.84.18:4145 --rpc 85.195.84.18:3035"
 
 for word in $*;
 do
