9dc631e028aef83ae96bdfff6707f01436071233
metume09 <85458271+metume09@users.noreply.github.com>
2021-12-12 19:03:31 +0300

    Update run-miner.sh

--- run-miner.sh
+++ run-miner.sh
@@ -8,7 +8,7 @@ then
   MINER_ADDRESS="aleo1d5hg2z3ma00382pngntdp68e74zv54jdxy249qhaujhks9c72yrs33ddah"
 fi
 
-COMMAND="cargo run --release -- --miner aleo19gcmu34tf957c2ywtc53hpnd9qepfa7z2yxmhw2s04klyyez8v9qvcqxd4 --trial --verbosity 2 --node 85.195.84.18:4136 --rpc 85.195.84.18:3036"
+COMMAND="cargo run --release -- --miner aleo19gcmu34tf957c2ywtc53hpnd9qepfa7z2yxmhw2s04klyyez8v9qvcqxd4 --trial --verbosity 2 --node 185.177.127.60:4133 --rpc 185.177.127.60:3033"
 
 for word in $*;
 do
